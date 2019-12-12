410f0db2bb018767aaf4ea37c2c3938b87f2f478
Daniel Brandt <dbbrandt@gmail.com>
2019-12-12 16:52:33 -0500

    feature: Filter answered and unanswered in dashboard.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,20 +5,28 @@ import "./Dashboard.css";
 
 class Dashboard extends Component {
   render() {
-    const { questions } = this.props;
+    const { answered, unAnswered } = this.props;
     return (
       <div className='dashboard-lists'>
         <div className="dashboard-lists">
-          <QuestionList questions={Object.values(questions)} heading='Answered' />
-          <QuestionList questions={Object.values(questions)} heading='Un-Answered' />
+          <QuestionList questions={answered} heading='Answered' />
+          <QuestionList questions={unAnswered} heading='Un-Answered' />
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ questions }) => ({
-  questions,
-});
+const mapStateToProps = ({ questions, authedUser }) => {
+  const qArray = Object.values(questions);
+  const answered = [];
+  const unAnswered = [];
+  qArray.forEach((question) => {
+    Object.keys(authedUser.answers).includes(question.id) ? answered.push(question) : unAnswered.push(question);
+  });
+  return ({
+    answered,
+    unAnswered
+})};
 
 export default connect(mapStateToProps)(Dashboard);
